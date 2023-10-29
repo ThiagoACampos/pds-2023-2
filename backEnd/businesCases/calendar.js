cb86f1270ea9623f9e2296dff7fa77153d38fc5f
Rodolfo <rodolfo.lins.cardoso@gmail.com>
2023-10-29 10:39:56 -0300

    Adjust to save the status

---
+++ backEnd/businesCases/calendar.js
@@ -0,0 +1,11 @@
+function getCalendarDays() {
+	const currentDate = new Date()
+	const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+	const day = currentDate.getDate()
+	const month = monthNames[currentDate.getMonth()]
+	
+
+}
+
+getCalendarDays()
